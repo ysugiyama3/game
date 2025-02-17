8754f07e34e0ba58482760cf37d3eb1a26af549d
Yukari Sugiyama <sugiyama_yukari@hotmail.com>
2025-02-17 11:28:08 -0500

    Update mainGametest01.js

--- js/mainGametest01.js
+++ js/mainGametest01.js
@@ -1,7 +1,7 @@
 const gameData = {
     "1": {
         "topic" : "Browser issue",
-        "text" : "A student has trouble accessing an ebook from their laptop from their residential college. They get routed to a 404-error message. But you cannot replicate the problem. What would you suggest them do first?",
+        "text" : "TEST!<br>A student has trouble accessing an ebook from their laptop from their residential college. They get routed to a 404-error message. But you cannot replicate the problem. What would you suggest them do first?",
         "linktext" : "",
         "link" : "",
         "image" : "smaller_images/snackies.png",
