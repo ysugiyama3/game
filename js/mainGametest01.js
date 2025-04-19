609b9a90e7efa81eddb097297c59f2c1beed3c2f
Yukari Sugiyama <sugiyama_yukari@hotmail.com>
2025-04-19 10:06:22 -0400

    Update mainGametest01.js
    test05

--- js/mainGametest01.js
+++ js/mainGametest01.js
@@ -1,7 +1,9 @@
+/* https://stackoverflow.com/questions/18990441/innerhtml-br-breaking */
+
 const gameData = {
     "1": {
         "topic" : "Browser issue",
-        "text" : "A student<br> has <strong>trouble</strong> accessing an ebook from their laptop from their residential college. They get routed to a 404 error message. But you cannot replicate the problem. What would you suggest them do first?",
+        "text" : "A student<br>\ has <strong>trouble</strong> accessing an ebook from their laptop from their residential college. They get routed to a 404 error message. But you cannot replicate the problem. What would you suggest them do first?",
         "linktext" : "",
         "link" : "",
         "image" : "smaller_images/1.PNG",
